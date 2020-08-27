677c513b267afbf558d2bff4e43e99652b4997a1
john doe <scottbruceburton@gmail.com>
2020-08-27 14:35:12 -0400

    working

--- tweet.py
+++ tweet.py
@@ -3,6 +3,20 @@
 
 import config
 import tweepy
+import requests
+import json
+
+SEPT11DEATHS = 2977
+
+# COVID Info Data
+url = "https://api.covidtracking.com/v1/us/daily.json"
+
+# Get Data from API
+r = requests.get(url)
+json_data = json.loads(r.text)
+
+print(json_data[1])
+
 
 # Authenticate to Twitter
 auth = tweepy.OAuthHandler(config.api_key, config.api_secret)
@@ -19,3 +33,21 @@ try:
     print("Authentication OK")
 except:
     print("Error during authentication")
+
+#  Prints the latest tweets from your Timeline
+# timeline = api.home_timeline()
+# for tweet in timeline:
+#     print(f"{tweet.user.name} said {tweet.text}\n")
+
+# Tweet from Account!
+# api.update_status("Test tweet from Tweepy Python")
+
+covidDeaths = json_data[0]['death']
+
+api.update_status(f'Quick Reminder! Today\'s US Covid Death Total: {covidDeaths}\n'
+                  f'That\'s like {covidDeaths / SEPT11DEATHS} 9/11\'s !\n'
+                  f'\n'
+                  f'\n'
+                  f'\n'
+                  f'\n Great Job America!')
+
