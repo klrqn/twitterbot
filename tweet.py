6bf6fca47f7a352293f01a3ac775de6c35aca319
john doe <scottbruceburton@gmail.com>
2020-08-27 18:43:42 -0400

    working! crontab'd

--- tweet.py
+++ tweet.py
@@ -6,6 +6,7 @@ import tweepy
 import requests
 import json
 
+
 SEPT11DEATHS = 2977
 
 # COVID Info Data
@@ -15,39 +16,40 @@ url = "https://api.covidtracking.com/v1/us/daily.json"
 r = requests.get(url)
 json_data = json.loads(r.text)
 
-print(json_data[1])
-
+# print(json_data[1])
 
 # Authenticate to Twitter
 auth = tweepy.OAuthHandler(config.api_key, config.api_secret)
 auth.set_access_token(config.access_token, config.token_secret)
-
-# Authenticate to Twitter
 # auth = tweepy.OAuthHandler("CONSUMER_KEY", "CONSUMER_SECRET")
 # auth.set_access_token("ACCESS_TOKEN", "ACCESS_TOKEN_SECRET")
 
+# connect to Twitter API
 api = tweepy.API(auth)
 
+# test connection
 try:
     api.verify_credentials()
     print("Authentication OK")
 except:
     print("Error during authentication")
+    print('\n')
 
-#  Prints the latest tweets from your Timeline
-# timeline = api.home_timeline()
-# for tweet in timeline:
-#     print(f"{tweet.user.name} said {tweet.text}\n")
-
-# Tweet from Account!
-# api.update_status("Test tweet from Tweepy Python")
-
+# Daily Covid Deaths
 covidDeaths = json_data[0]['death']
 
-api.update_status(f'Quick Reminder! Today\'s US Covid Death Total: {covidDeaths}\n'
-                  f'That\'s like {covidDeaths / SEPT11DEATHS} 9/11\'s !\n'
-                  f'\n'
-                  f'\n'
-                  f'\n'
-                  f'\n Great Job America!')
+# Tweet Covid Update
+# api.update_status(f'Quick Reminder! Today\'s US Covid Death Total: {covidDeaths}\n'
+#                   f'That\'s like {"{:.2f}".format(covidDeaths / SEPT11DEATHS)} 9/11\'s !\n'
+#                   f'\n'
+#                   f'\n'
+#                   f'\n'
+#                   f'\n Great Job America!')
 
+api.update_status(f'Just a quick reminder:\n'
+      f'Today\'s US Covid Death Total is {covidDeaths}\n'
+      f'That\'s like {"{:.0f}".format(covidDeaths / SEPT11DEATHS)} 9/11s!\n'
+      f'.\n.\n.\n'
+      f'Great Job America!')
+
+# "{:.2f}".format(a_float)
